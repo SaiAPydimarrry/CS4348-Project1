3964ce36ede4e6de92e6f2da42b2b1940d01ab57
ange-222 <145289718+ange-222@users.noreply.github.com>
2024-10-19 11:51:28 -0500

    Update TaskExecutorImpl.java

--- TaskExecutorStudentDevProject/src/edu/utdallas/taskExecutorImpl/TaskExecutorImpl.java
+++ TaskExecutorStudentDevProject/src/edu/utdallas/taskExecutorImpl/TaskExecutorImpl.java
@@ -5,16 +5,30 @@ import edu.utdallas.taskExecutor.TaskExecutor;
 
 public class TaskExecutorImpl implements TaskExecutor
 {
+	private final BlockingQueue taskQueue; 
+	private final Thread[] workerThreads; //threadpool array
 
 	public TaskExecutorImpl(int threadPoolSize)
 	{
 		// TODO Complete the implementation
+		taskQueue = new BlockingQueue(100); //max size of queue is 100
+		workerThreads = new Thread [threadPoolSize]; //create n space for thread pool
+
+		for(int i = 0; i<threadPoolSize; i++){ //iterate over threadpool
+			workerThreads[i] = new Thread(new TaskRunner(taskQueue)); //create new thread and  fetch task
+			workerThreads[i].start(); //start thread
+		}
 	}
 	
 	@Override
 	public void addTask(Task task)
 	{
 		// TODO Complete the implementation
+		try{
+			taskQueue.put(task); //add task to queue, block if queue is full
+		}catch(InterruptedException e){
+			Thread.currentThread().interrupt();
+		}
 	}
 
 }
